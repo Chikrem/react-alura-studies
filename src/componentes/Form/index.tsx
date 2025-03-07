042227a02efeee71f38c1af12e6e266b63e22acc
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-03-07 10:45:54 -0300

    Controle de Formulário

--- src/componentes/Form/index.tsx
+++ src/componentes/Form/index.tsx
@@ -3,15 +3,28 @@ import Botao from "../Botao";
 import style from "./Form.module.scss";
 
 class Formulario extends React.Component {
+  
+  state = {
+    tarefa: "",
+    tempo: "00:00:00",
+  };
+
+  adicionarTarefa(evento : React.FormEvent) {
+    evento.preventDefault();
+    console.log('state', this.state);
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione uma nova tarefa</label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.tarefa}
+            onChange={(event) => this.setState({...this.state, tarefa: event.target.value})}
             placeholder="Digite a tarefa"
             required
           />
@@ -23,6 +36,8 @@ class Formulario extends React.Component {
             step="1"
             name="tempo"
             id="tempo"
+            value={this.state.tempo}
+            onChange={(event) => this.setState({...this.state, tempo: event.target.value})}
             min="00:00:00"
             max="01:30:00"
             required
